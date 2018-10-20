926638bb44714102b60224d53c2ce6f6f0eb0686
fyxxf <44317465+fyxxf@users.noreply.github.com>
2018-10-20 16:48:06 +0300

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -1,9 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const ayarlar = require('./ayarlar.json');
+
+var prefix = ayarlar.prefix;
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`BOT: ${client.user.username} adı ile giriş yaptı!`);
+});
 
+client.on('message', msg => {
+  console.log(`LOG: S: ${msg.guild.name} M: ${msg.content} Y: ${msg.author.tag}`);
+  if (msg.author.id === ayarlar.id) return;
+  if (msg.author.bot) return;
+
+  if (!msg.content.startsWith(prefix)) {
+	  return;
   }
   if (msg.content.toLowerCase() === prefix + 'ping') {
     msg.reply('Pong! **' + client.ping + '** ms');
@@ -36,4 +47,4 @@ client.on('ready', () => {
   }
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(ayarlar.token);
