8b5675bc75c8178bdc679f9da492cca3842837c2
fyxxf <44317465+fyxxf@users.noreply.github.com>
2018-10-20 16:42:41 +0300

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -1,14 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const Discord = require('discord.js');
+exports.run = function(client, message, args) {
+  message.reply('Ping Değerim: **' + client.ping + '** ms');
+};
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong! ** + ${client.ping} + ** ms');
-  }
-});
 
 client.login(process.env.BOT_TOKEN);
