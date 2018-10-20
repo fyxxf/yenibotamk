3eb05899e38608cb38b0c20c7ed7a30be369434c
fyxxf <44317465+fyxxf@users.noreply.github.com>
2018-10-20 16:39:26 +0300

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
-    msg.reply('Pong! ** + client.ping + ** ms');
+    msg.reply('Pong! ** + ${client.ping} + ** ms');
   }
 });
 
